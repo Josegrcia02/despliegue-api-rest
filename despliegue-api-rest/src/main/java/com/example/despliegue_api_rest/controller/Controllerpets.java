cd43b19fee7a8fbfe8a1a76537b32783e10a4720
Josegrcia02 <josefol881@gmail.com>
2025-09-23 21:46:57 +0200

    listado pets

--- despliegue-api-rest/src/main/java/com/example/despliegue_api_rest/controller/Controllerpets.java
+++ despliegue-api-rest/src/main/java/com/example/despliegue_api_rest/controller/Controllerpets.java
@@ -1,13 +1,19 @@
 package com.example.despliegue_api_rest.controller;
 
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.despliegue_api_rest.entity.Pet;
+import com.example.despliegue_api_rest.repository.PetRepository;
 
 @RestController
 public class Controllerpets {
-
+    private PetRepository petRepository;
+    public Controllerpets(PetRepository petRepository){
+        this.petRepository = petRepository;
+    }
     @GetMapping
-    public String hello(){
-        return "Hello World";
+    public List<Pet> ListadoPets(){
+        return petRepository.findAll();
     }
 }
