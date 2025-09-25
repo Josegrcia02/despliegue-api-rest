9c836bb056030d4ed0b0400951ff2abe9f3bba40
Josegrcia02 <josefol881@gmail.com>
2025-09-25 17:45:12 +0200

    configuración de la busqueda

--- despliegue-api-rest/src/main/java/com/example/despliegue_api_rest/controller/Controllerpets.java
+++ despliegue-api-rest/src/main/java/com/example/despliegue_api_rest/controller/Controllerpets.java
@@ -2,17 +2,19 @@ package com.example.despliegue_api_rest.controller;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.despliegue_api_rest.entity.Pet;
 import com.example.despliegue_api_rest.repository.PetRepository;
 
 @RestController
+@RequestMapping("/pet")
 public class Controllerpets {
     private PetRepository petRepository;
     public Controllerpets(PetRepository petRepository){
         this.petRepository = petRepository;
     }
-    @GetMapping ("/Pets")
+    @GetMapping ("/list")
     public List<Pet> ListadoPets(){
         return petRepository.findAll();
     }
